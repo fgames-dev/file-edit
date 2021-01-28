69e59c0c7d7551983ed923e245ea72a1d8e1ce3d
fgames-dev <70545380+fgames-dev@users.noreply.github.com>
2021-01-28 09:29:33 -0600

    Add files via upload

---
+++ fileedit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+read -p "File to create (or edit): " file
+read -p "Erase file before editing? Y/N " er
+if [ $er = "n" ] || [ $er = "N" ]
+then
+read -p "Text to append: " text
+echo $text >> $file
+echo "File appended"
+fi
+if [ $er = "y" ] || [ $er = "Y" ]
+then
+rm $file
+read -p "File contents: " txt
+echo $txt >> $file
+fi
+echo "Program complete."
