9fccefbb408077ce2d77fb75ecd1caa2a3a1f552
fgames-dev <70545380+fgames-dev@users.noreply.github.com>
2021-02-12 10:38:37 -0600

    Set to append instead of replace
    Will now append text without erasing file contents.

--- write.php
+++ write.php
@@ -1,9 +1,8 @@
 <?php
 $filename = $_GET['filename'];
-$myfile = fopen($filename, "w") or die("Unable to open file!");
+$myfile = fopen($filename, "a") or die("Unable to open file!");
 $data = $_GET['data'];
 $txt = $_GET['text'];
-$ello = 'ello';
 fwrite($myfile, $txt);
 fclose($myfile);
 header("Location: index.html");
